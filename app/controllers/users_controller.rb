dcfba0a98dbd8416bcdb1b517177df334ca12cf9
Martin Soler <martinrsoler@yahoo.com>
2017-04-10 11:14:31 -1000

    Listed, added current_user to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+    @user = current_user
     redirect_to root_path
   end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:@user) { create(:user, email: "listed@email.com") }
 
   before(:each) do
     @user = FactoryGirl.create(:user)
