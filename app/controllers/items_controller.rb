9a187619f0fe8ac785343917485c65935850db20
Martin Soler <martinrsoler@yahoo.com>
2017-04-14 22:18:12 -1000

    Listed to do items, working create and show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,23 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     @item.user = current_user
 
     if @item.save
       flash[:notice]  = "To do item was saved."
-      redirect_to [@user, @items]
+      redirect_to users_show_path
     else
       flash.now[:alert] = "Sorry, there was an error saving the to do item. Please try again."
       render.new
     end
-
-    private
-
-    def item_params
-      params.require(:item).permit(:name)
-    end
-
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
