98906992a8d34c04612d7009b7d2a4ed2d808832
Martin Soler <martinrsoler@yahoo.com>
2017-04-16 14:22:30 -1000

    Listed seeding data, completed. Tested logging on to see user to do items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Create Users
+5.times do
 
 User.create!(
-  email: 'mentor@bloc.io',
-  password: 'helloworld'
-)
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password(8)
+  )
+end
+users = User.all
+
+# Create To Do Items
+25.times do
+  Item.create!(
+    name: Faker::StarWars.quote,
+    user: users.sample
+  )
+end
+
+puts "Finished seeding"
+puts "#{User.count} users created"
+puts "#{Item.count} to do items created"
